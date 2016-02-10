a5cae366a6aefbd728fe2d01e32753861d9d8974
Max Rydahl Andersen <manderse@redhat.com>
2016-02-10 10:47:04 +0100

    first scout bugzilla
    How to call:
    python scout-bugzilla.py -s https://bugs.eclipse.org -q status_whiteboard=RHT

---
+++ scouts/scout-bugzilla.py
@@ -0,0 +1,38 @@
+import bugzilla
+from optparse import OptionParser
+import json
+
+def parse_options():
+    usage = "Usage: %prog -u <user> -p <password> \nBugzilla Scout"
+
+    parser = OptionParser(usage)
+    parser.add_option("-u", "--user", dest="username", help="bz username")
+    parser.add_option("-p", "--pwd", dest="password", help="bz password")
+    parser.add_option("-s", "--server", dest="server", help="bz instance")
+    parser.add_option("-q", "--query", dest="query", help="bz query")
+    (options, args) = parser.parse_args()
+
+    return options
+
+options = parse_options()
+
+bz = bugzilla.Bugzilla(url=options.server + "/bugs/xmlrpc.cgi")
+
+querystr = options.server + "/bugs/buglist.cgi?" + options.query
+
+queryobj = bz.url_to_query(querystr)
+
+issues = bz.query(queryobj)
+
+json_issues = []
+for bug in issues:
+    jq = {} 
+    jq["id"] = bug.id
+    jq["weburl"] = bug.weburl
+    jq["summary"] = bug.summary
+    jq["project"] = bug.product
+    jq["components"] = [{ "name" : bug.component }] 
+    json_issues.append(jq)
+
+print json.dumps(json_issues, sort_keys=True, indent=4)
+    
