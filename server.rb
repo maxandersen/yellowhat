b4d6f8ade92e1ef6d4ec5c8cba88f97871722caa
Aslak Knutsen <aslak@4fs.no>
2016-02-10 12:56:47 +0100

    Add Server/Engine

--- server.rb
+++ server.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'sinatra/json'
+require_relative 'engine.rb'
+
+engine = YellowHat::Engine.new (YellowHat::Config.new('config/uberprojects.yaml'))
 
 set :public_folder, File.dirname(__FILE__) + '/app'
 
@@ -7,7 +11,19 @@ get '/' do
 end
 
 get '/uberprojects' do
-   {:id => 'a'}
+   json engine.uber_projects
+end
+
+get '/uberprojects/:id' do
+   id = params['id']
+   unless id
+      status 404
+      body({'type' => 'error', 'message' => 'not_found'})
+      return
+   end
+   data = engine.collect(id)
+   return json({'type' => 'error', 'message' => 'unknown_uber_project'}) if data.nil?
+   json data
 end
 
 
